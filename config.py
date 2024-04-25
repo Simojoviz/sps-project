ca1c9dd58165a596715190f654a0a92b80eefd7a
Simojoviz <simonejov@gmail.com>
2024-04-25 17:10:17 +0200

    sqlite3 as DBMS

--- config.py
+++ config.py
@@ -2,11 +2,9 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
-
-
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'rnd-secret-key'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://username:password@localhost/films'
+    SQLALCHEMY_DATABASE_URI = f'sqlite:///{basedir}/data/database.sqlite3'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
